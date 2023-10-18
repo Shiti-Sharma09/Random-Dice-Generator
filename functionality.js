1a4956b3f3957ac65cab40d4e17220fe34758b36
Shiti Sharma <113706711+shitaayyy@users.noreply.github.com>
2023-10-18 23:16:59 +0530

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -1,22 +1,22 @@
 var randomNumber1 = Math.random();
 var new1 = Math.floor(randomNumber1*6) + 1;
 if (new1 == 1) {
-    document.getElementById("die1").setAttribute('src','./imgg/dice1.png');
+    document.getElementById("die1").setAttribute('src','dice1.png');
 }
 else if (new1 == 2) {
-    document.getElementById("die1").setAttribute('src','./imgg/dice2.png');
+    document.getElementById("die1").setAttribute('src','dice2.png');
 }
 else if (new1 == 3) {
-    document.getElementById("die1").setAttribute('src','./imgg/dice3.png');
+    document.getElementById("die1").setAttribute('src','dice3.png');
 }
 else if (new1 == 4) {
-    document.getElementById("die1").setAttribute('src','./imgg/dice4.png');
+    document.getElementById("die1").setAttribute('src','dice4.png');
 }
 else if (new1 == 5) {
-    document.getElementById("die1").setAttribute('src','./imgg/dice5.png');
+    document.getElementById("die1").setAttribute('src','dice5.png');
 }
 else if (new1 == 6) {
-    document.getElementById("die1").setAttribute('src','./imgg/dice6.png');
+    document.getElementById("die1").setAttribute('src','dice6.png');
 }
 
 
@@ -24,22 +24,22 @@ else if (new1 == 6) {
 var randomNumber2 = Math.random();
 var new2 = Math.floor(randomNumber2*6) + 1;
 if (new2 == 1) {
-    document.getElementById("die2").setAttribute('src','./imgg/dice1.png');
+    document.getElementById("die2").setAttribute('src','dice1.png');
 }
 else if (new2 == 2) {
-    document.getElementById("die2").setAttribute('src','./imgg/dice2.png');
+    document.getElementById("die2").setAttribute('src','dice2.png');
 }
 else if (new2 == 3) {
-    document.getElementById("die2").setAttribute('src','./imgg/dice3.png');
+    document.getElementById("die2").setAttribute('src','dice3.png');
 }
 else if (new2 == 4) {
-    document.getElementById("die2").setAttribute('src','./imgg/dice4.png');
+    document.getElementById("die2").setAttribute('src','dice4.png');
 }
 else if (new2 == 5) {
-    document.getElementById("die2").setAttribute('src','./imgg/dice5.png');
+    document.getElementById("die2").setAttribute('src','dice5.png');
 }
 else {
-    document.getElementById("die2").setAttribute('src','./imgg/dice6.png');
+    document.getElementById("die2").setAttribute('src','dice6.png');
 }
 
 
